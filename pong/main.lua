a592e412c806c9faf39cdd0845ac7fab9ad750c7
hallowf <alexandre.cortegaca@gmail.com>
2020-03-30 19:30:04 +0100

    pong-11

--- pong/main.lua
+++ pong/main.lua
@@ -35,6 +35,12 @@ function love.load()
   largeFont = love.graphics.newFont('Retron2000.ttf', 16)
   scoreFont = love.graphics.newFont('Retron2000.ttf', 22)
 
+  sounds = {
+    ['paddle_hit'] = love.audio.newSource('sounds/paddle_hit.wav', 'static'),
+    ['score'] = love.audio.newSource('sounds/score.wav', 'static'),
+    ['wall_hit'] = love.audio.newSource('sounds/wall_hit.wav', 'static')
+  }
+
   push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
     fullscreen = false,
     resizable = false,
@@ -86,6 +92,8 @@ function love.update(dt)
       else
         ball.dy = math.random(10, 150)
       end
+
+      sounds['paddle_hit']:play()
     end
 
     if ball:collides(player2) then
@@ -97,18 +105,23 @@ function love.update(dt)
       else
         ball.dy = math.random(10, 150)
       end
+
+      sounds['paddle_hit']:play()
     end
 
     --detect upper and lower boundaries and reverse if colliding
     if ball.y <=  0 then
       ball.y = 0
       ball.dy = -ball.dy
+
+      sounds['wall_hit']:play()
     end
 
     -- -4 to account for the ball's size
     if ball.y >= VIRTUAL_HEIGHT - 4 then
       ball.y = VIRTUAL_HEIGHT - 4
       ball.dy  = -ball.dy
+      sounds['wall_hit']:play()
     end
 
     if ball.x < 0 then
@@ -121,6 +134,7 @@ function love.update(dt)
         gameState = 'serve'
         ball:reset()
       end
+      sounds['score']:play()
     end
   
     if ball.x > VIRTUAL_WIDTH then
@@ -133,6 +147,7 @@ function love.update(dt)
         gameState = 'serve'
         ball:reset()
       end
+      sounds['score']:play()
     end
   end
 
