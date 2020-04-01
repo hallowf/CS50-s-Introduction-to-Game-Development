c4654fc7ec7b7230ce88db70df64b957118ea005
hallowf <alexandre.cortegaca@gmail.com>
2020-04-01 16:13:17 +0100

    flappy-9

--- flappy_bird/main.lua
+++ flappy_bird/main.lua
@@ -11,6 +11,7 @@ require 'PipePair'
 require 'StateMachine'
 require 'states/BaseState'
 require 'states/PlayState'
+require 'states/ScoreState'
 require 'states/TitleScreenState'
 
 WINDOW_WIDTH = 1280
@@ -59,6 +60,7 @@ function love.load()
   gStateMachine = StateMachine {
     ['title'] = function() return TitleScreenState() end,
     ['play'] = function() return PlayState() end,
+    ['score'] = function() return ScoreState() end
   }
   gStateMachine:change('title')
 
